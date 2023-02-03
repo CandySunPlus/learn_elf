ae7f999a61f36018decb3a06ce968dddfeed7fde
niksun <candysfm@gmail.com>
2023-02-04 02:08:26 +0800

    add memory map

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ use std::{
     process::{Command, Stdio},
 };
 
-use mmap::{MapOption, MemoryMap};
-use region::{protect, Protection};
-
 mod process;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -15,150 +12,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("Analyzing {input_path:?}...");
     let mut proc = process::Process::new();
-    let obj = proc.load_object(input_path);
-    println!("{obj:#?}");
+    proc.load_object_and_dependencies(input_path)?;
 
-    let file = &obj.file;
-
-    let rela_entries = file.read_rela_entries().unwrap_or_else(|e| {
-        println!("Could not read relocations: {e:?}");
-        Default::default()
-    });
-
-    println!("Found {} rela entries", rela_entries.len());
-
-    for entry in &rela_entries {
-        println!("{entry:?}");
-    }
-
-    let syms = file.read_syms().unwrap_or_else(|e| {
-        println!("Could not read symbol tables: {e:?}");
-        Default::default()
-    });
-
-    println!(
-        "Symbol table @ {:?} contains {} entries",
-        file.dynamic_entry(delf::DynamicTag::SymTab).unwrap(),
-        syms.len()
-    );
-    println!(
-        "  {:6}{:12}{:10}{:16}{:16}{:12}{:12}",
-        "Num", "Value", "Size", "Type", "Bind", "Ndx", "Name"
-    );
-    for (num, s) in syms.iter().enumerate() {
-        println!(
-            "  {:6}{:12}{:10}{:16}{:16}{:12}{:12}",
-            format!("{num}"),
-            format!("{:?}", s.value),
-            format!("{:?}", s.size),
-            format!("{:?}", s.r#type),
-            format!("{:?}", s.bind),
-            format!("{:?}", s.shndx),
-            format!("{}", file.get_string(s.name).unwrap_or_default()),
-        );
-    }
-
-    let msg = syms
-        .iter()
-        .find(|sym| file.get_string(sym.name).unwrap_or_default() == "msg")
-        .expect("should find msg in symbol table");
-    let msg_slice = file.slice_at(msg.value).expect("shoud find msg in memory");
-    let msg_slice = &msg_slice[..msg.size as _];
-
-    println!("msg contents: {:?}", String::from_utf8_lossy(msg_slice));
-
-    let base = 0x400000_usize;
-
-    println!("Loading with base address @ 0x{base:x}");
-
-    let mut mappings = Vec::new();
-
-    let phs = file
-        .program_headers
-        .iter()
-        .filter(|ph| ph.r#type == delf::SegmentType::Load)
-        .filter(|ph| ph.mem_range().end > ph.mem_range().start);
-
-    for ph in phs {
-        println!("Mapping {:?} - {:?}", ph.mem_range(), ph.flags);
-        let mem_range = ph.mem_range();
-        let len: usize = (mem_range.end - mem_range.start).into();
-
-        let start = mem_range.start.0 as usize + base;
-        let aligned_start = align_to(start);
-        let padding = start - aligned_start;
-        let len = len + padding;
-
-        let addr = aligned_start as *mut u8;
-
-        if padding > 0 {
-            println!("(With 0x{padding:08x} bytes of padding at the start)");
-        }
-
-        let map = MemoryMap::new(len, &[MapOption::MapWritable, MapOption::MapAddr(addr)])?;
-
-        unsafe {
-            std::ptr::copy_nonoverlapping(ph.data.as_ptr(), addr.add(padding), len);
-        }
-
-        let mut num_relocs = 0;
-        for reloc in &rela_entries {
-            if mem_range.contains(&reloc.offset) {
-                num_relocs += 1;
-                unsafe {
-                    let real_segment_start = addr.add(padding);
-                    let specified_reloc_offset = reloc.offset;
-                    let specified_segment_start = mem_range.start;
-                    let offset_into_segment = specified_reloc_offset - specified_segment_start;
-                    let reloc_addr = real_segment_start.add(offset_into_segment.into()) as *mut u64;
-
-                    match reloc.r#type {
-                        delf::RelType::Known(t) => {
-                            num_relocs += 1;
-                            match t {
-                                delf::KnownRelType::Relative => {
-                                    let reloc_value = reloc.addend + base.into();
-                                    *reloc_addr = reloc_value.0;
-                                }
-                                t => {
-                                    panic!("Unsupported relocation type {t:?}");
-                                }
-                            }
-                        }
-                        delf::RelType::Unknown(_) => {}
-                    }
-                }
-            }
-        }
-
-        if num_relocs > 0 {
-            println!("(Applied {num_relocs} relocations)");
-        }
-
-        let mut protection = Protection::NONE;
-
-        for flag in ph.flags.iter() {
-            protection |= match flag {
-                delf::SegmentFlag::Read => Protection::READ,
-                delf::SegmentFlag::Write => Protection::WRITE,
-                delf::SegmentFlag::Execute => Protection::EXECUTE,
-            }
-        }
-
-        unsafe {
-            protect(addr, len, protection)?;
-        }
-
-        mappings.push(map);
-    }
-
-    let new_entry_point = (file.entry_point + base.into()).into();
-
-    println!("Jumping to entry point @ {new_entry_point:?}...");
-
-    unsafe {
-        jmp(new_entry_point);
-    }
+    println!("{proc:#?}");
 
     Ok(())
 }
@@ -192,11 +48,13 @@ fn ndisasm(code: &[u8], origin: delf::Addr) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[allow(dead_code)]
 unsafe fn jmp(addr: *const u8) {
     let fn_ptr: fn() = std::mem::transmute(addr);
     fn_ptr();
 }
 
+#[allow(dead_code)]
 fn align_to(x: usize) -> usize {
     x & !0xFFF
 }
--- src/process.rs
+++ src/process.rs
@@ -1,10 +1,15 @@
 use std::{
+    cmp::{max, min},
+    collections::HashMap,
     fs,
+    io::Read,
+    ops::Range,
+    os::fd::AsRawFd,
     path::{Path, PathBuf},
 };
 
 use custom_debug_derive::Debug as CustomDebug;
-use mmap::MemoryMap;
+use mmap::{MapOption, MemoryMap};
 
 #[derive(CustomDebug)]
 pub struct Object {
@@ -14,44 +19,183 @@ pub struct Object {
     pub file: delf::File,
     #[debug(skip)]
     pub maps: Vec<MemoryMap>,
+    pub mem_range: Range<delf::Addr>,
 }
 
 #[derive(Debug)]
 pub struct Process {
     pub objects: Vec<Object>,
+    pub objects_by_path: HashMap<PathBuf, usize>,
     pub search_path: Vec<PathBuf>,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum LoadError {
+    #[error("ELF object not found: {0}")]
+    NotFound(String),
+    #[error("An invalid or unsupoported path was encountered")]
+    InvalidPath(PathBuf),
+    #[error("I/O error on {0}: {1}")]
+    IO(PathBuf, std::io::Error),
+    #[error("ELF object could not be parsed: {0}")]
+    ParseError(PathBuf),
+    #[error("ELF object has no load segments")]
+    NoLoadSegments,
+    #[error("ELF object could not be mapped in memory: {0}")]
+    MapError(#[from] mmap::MapError),
+}
+
+pub enum GetResult {
+    Cached(usize),
+    Fresh(usize),
+}
+
+impl GetResult {
+    fn fresh(self) -> Option<usize> {
+        if let Self::Fresh(index) = self {
+            Some(index)
+        } else {
+            None
+        }
+    }
 }
 
 impl Process {
     pub fn new() -> Self {
         Self {
             objects: Default::default(),
-            search_path: Default::default(),
+            objects_by_path: Default::default(),
+            search_path: vec!["/usr/lib".into()],
         }
     }
 
-    pub fn load_object<P: AsRef<Path>>(&mut self, path: P) -> &Object {
-        let path = path.as_ref().canonicalize().unwrap();
-        let input = fs::read(&path).unwrap();
+    pub fn object_path(&self, name: &str) -> Result<PathBuf, LoadError> {
+        self.search_path
+            .iter()
+            .filter_map(|p| p.join(name).canonicalize().ok())
+            .find(|p| p.exists())
+            .ok_or_else(|| LoadError::NotFound(name.into()))
+    }
+
+    pub fn get_object(&mut self, name: &str) -> Result<GetResult, LoadError> {
+        let path = self.object_path(name)?;
+        self.objects_by_path
+            .get(&path)
+            .map(|&index| Ok(GetResult::Cached(index)))
+            .unwrap_or_else(|| self.load_object(path).map(GetResult::Fresh))
+    }
+
+    pub fn load_object_and_dependencies<P: AsRef<Path>>(
+        &mut self,
+        path: P,
+    ) -> Result<usize, LoadError> {
+        let index = self.load_object(path)?;
+
+        let mut a = vec![index];
+
+        while !a.is_empty() {
+            a = a
+                .into_iter()
+                .map(|index| &self.objects[index].file)
+                .flat_map(|file| file.dynamic_entry_strings(delf::DynamicTag::Needed))
+                .collect::<Vec<_>>()
+                .into_iter()
+                .map(|dep| self.get_object(&dep))
+                .collect::<Result<Vec<_>, _>>()?
+                .into_iter()
+                .filter_map(GetResult::fresh)
+                .collect()
+        }
+
+        Ok(index)
+    }
+
+    pub fn load_object<P: AsRef<Path>>(&mut self, path: P) -> Result<usize, LoadError> {
+        let path = path
+            .as_ref()
+            .canonicalize()
+            .map_err(|e| LoadError::IO(path.as_ref().to_path_buf(), e))?;
+
+        let mut fs_file = fs::File::open(&path).map_err(|e| LoadError::IO(path.clone(), e))?;
+        let mut input = Vec::new();
+
+        fs_file
+            .read_to_end(&mut input)
+            .map_err(|e| LoadError::IO(path.clone(), e))?;
 
         println!("Loading {path:?}");
-        let file = delf::File::parse_or_print_error(&input[..]).unwrap();
 
-        let origin = path.parent().unwrap().to_str().unwrap();
+        let file = delf::File::parse_or_print_error(&input[..])
+            .ok_or_else(|| LoadError::ParseError(path.clone()))?;
 
-        for rpath in file.dynamic_entry_strings(delf::DynamicTag::RPath) {
-            let rpath = rpath.replace("$ORIGIN", &origin);
-            println!("Found RPATH entry {rpath:?}");
-            self.search_path.push(PathBuf::from(rpath));
-        }
+        let origin = path
+            .parent()
+            .and_then(|p| p.to_str())
+            .ok_or_else(|| LoadError::InvalidPath(path.clone()))?;
+
+        self.search_path.extend(
+            file.dynamic_entry_strings(delf::DynamicTag::RPath)
+                .map(|p| p.replace("$ORIGIN", origin))
+                .inspect(|p| println!("Found RPATH entry {p:?}"))
+                .map(PathBuf::from),
+        );
+
+        let deps = file
+            .dynamic_entry_strings(delf::DynamicTag::Needed)
+            .collect::<Vec<_>>();
+
+        let load_segments = || {
+            file.program_headers
+                .iter()
+                .filter(|ph| ph.r#type == delf::SegmentType::Load)
+        };
+
+        let mem_range = load_segments()
+            .map(|ph| ph.mem_range())
+            .fold(None, |acc, range| match acc {
+                None => Some(range),
+                Some(acc) => Some(convex_hull(acc, range)),
+            })
+            .ok_or(LoadError::NoLoadSegments)?;
+
+        let mem_size = (mem_range.end - mem_range.start).into();
+        let mem_map = MemoryMap::new(mem_size, &[])?;
+        let base = delf::Addr(mem_map.data() as _) - mem_range.start;
+
+        let maps = load_segments()
+            .map(|ph| {
+                println!("Mapping {ph:#?}");
+                MemoryMap::new(
+                    ph.memsz.into(),
+                    &[
+                        MapOption::MapFd(fs_file.as_raw_fd()),
+                        MapOption::MapOffset(ph.offset.into()),
+                        MapOption::MapAddr(unsafe { (base + ph.vaddr).as_ptr() }),
+                    ],
+                )
+            })
+            .collect::<Result<_, _>>()?;
+
+        let index = self.objects.len();
 
         let object = Object {
-            path,
-            base: delf::Addr(0x400000),
-            maps: Default::default(),
+            path: path.clone(),
+            base,
+            maps,
             file,
+            mem_range,
         };
         self.objects.push(object);
-        self.objects.last().unwrap()
+        self.objects_by_path.insert(path, index);
+
+        for dep in &deps {
+            self.get_object(dep)?;
+        }
+
+        Ok(index)
     }
+}
+
+fn convex_hull(a: Range<delf::Addr>, b: Range<delf::Addr>) -> Range<delf::Addr> {
+    (min(a.start, b.start))..(max(a.end, b.end))
 }
